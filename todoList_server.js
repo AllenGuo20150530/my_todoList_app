52fd0c6b67e480d54f6313a57ed20436eb1a3b53
AllenGuo20150530 <guoyu2015guoallen@hotmail.com>
2016-11-02 10:19:31 +0800

    完成所有阿贾克斯操作

--- todoList_server.js
+++ todoList_server.js
@@ -1,51 +1,27 @@
 var express = require('express')
 var app = express()
-
 var bodyParser = require('body-parser');
 // parse application/json
 app.use(bodyParser.json())
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
-
-// use static files
+// setup static files
 app.use(express.static('static'))
 
-app.post('/todo/add', function(request, response){
-    console.log('request.body-->', request.body)
-    var data = {
-        'id': request.body.id,
-        'task': request.body.task,
-        'created_time': request.body.created_time,
-    }
-    console.log(request.body.id, request.body.task)
-    var r = JSON.stringify(data)
-    response.send(r)
-})
-// app.post('/todo/add', function(request, response){
-//     console.log('request.body-->', request.body)
-//     response.send("post request")
-// })
+
 
 app.get('/', function(request, response){
-    // // 读取首页消息
-    // var options = {
-    //      encoding: 'utf-8'
-    // }
-    //
-    // var fs = require('fs')
-    // fs.readFile('index.html', options, function(err, data){
-    //     console.log('读取の内容是：', data)
-    //     response.send(data)
-    // })
     var path = 'index.html'
     sendHtml(path, response)
 })
-
+// 读取HTML文件内容并发送
 var sendHtml = function(path, response){
+    // 编码utf-8
     var options = {
          encoding: 'utf-8'
     }
-
+    // 请求fs：file system 文件系统
+    // fs 是 node 中处理文件和目录的库
     var fs = require('fs')
     fs.readFile(path, options, function(err, data){
         console.log('读取の内容是：', data)
@@ -53,30 +29,96 @@ var sendHtml = function(path, response){
     })
 }
 
+var todos = [
+    {
+        "id": 1,
+        "task": "study",
+    }
+]
+// 往todos中添加新todo
+var addTodo = function(todo) {
+    // todo = { task: "study"} 形式
+    // todos empty or not
+    var len = todos.length
+    if(len > 0) {
+        todo.id = todos[len - 1].id + 1
+    }else {
+        todo.id = 1
+    }
+    todos.push(todo)
+}
 
-app.get('/todo/all', function(request, response){
-    var all = [
-        {
-            "created_time": 1476083080,
-            "id": 75,
-            "task": "to have dinner"
+// 通过id来查找到todo, 并返回其在Todos中的下标
+var todoById = function(id) {
+    var index = -1
+    for (var i = 0; i < todos.length; i++) {
+        var t = todos[i]
+        if(t.id == id) {
+            index = i
         }
-    ]
-    var data = JSON.stringify(all)
+    }
+    return index
+}
+
+// 通过id来删除某个todo
+var deleteTodo = function(id) {
+    var index = todoById(id)
+    if (index != -1) {
+        todos.splice(index, 1)
+        return '删除成功！'
+    }else if (index === -1) {
+        return '没有相关任务！'
+    }
+}
+
+// 通过id来更新相对应的todo
+var updateTodo = function(todo) {
+    var index = todoById(todo.id)
+    if (index != -1) {
+        var todoUpdate = todos[index]
+        todoUpdate.task = todo.task
+        return '更新成功！'
+    }else if (index === -1) {
+        return '没有相关任务！'
+    }
+}
+
+// query all todos
+app.get('/todo/all', function(request, response){
+    var data = JSON.stringify(todos)
     response.send(data)
 })
 
-// 传入文件
-// var sendDoc = fucntion(path, docName) {
-//     app.get('/' + docName, function (req, res) {
-//         console.log('__dirname-->', __dirname)
-//         res.sendFile( __dirname + path + docName );
-//     })
-// }
-//
-// sendDoc('/', 'todo_list.js')
+// add todo
+app.post('/todo/add', function(request, response){
+    console.log('request.body-->', request.body)
+    var todo = request.body
+    console.log(request.body.task)
+    addTodo(todo)
+    console.log(todos)
+    var data = JSON.stringify(todos)
+    console.log(data)
+    response.send(data)
+})
 
-// 将jQuery库和todo_list.js文件引入
+// update todo
+app.post('/todo/update', function(request, response){
+    console.log('request.body-->', request.body)
+    var todo = request.body
+    console.log('todo-->', todo)
+    console.log(updateTodo(todo))
+})
+
+// delete todo
+app.post('/todo/delete', function(request, response){
+    console.log('request.body-->', request.body)
+    var id = Number(request.body.id)
+    console.log('id-->', id)
+    console.log(deleteTodo(id))
+})
+
+
+// 将jQuery库和todo_list.js文件引入, 如此才能在index.html中引用这两个文件
 app.get('/todo_list.js', function (req, res) {
     console.log('__dirname-->', __dirname)
     res.sendFile( __dirname + "/" + "todo_list.js" );
@@ -86,8 +128,9 @@ app.get('/node_modules/jquery/dist/jquery.min.js', function (req, res) {
     res.sendFile( __dirname + "/node_modules/jquery/dist/jquery.min.js" );
 })
 
-
+// 开启server，端口8081
 var server = app.listen(8081, function(){
+
     var host = server.address().host
     var port = server.address().port
 
--- todo_list.js
+++ todo_list.js
@@ -3,59 +3,58 @@ var log = function() {
     console.log.apply(console, arguments)
 }
 //
-// 插入TODOlist区域
-var todoList = function() {
-    var todoContainer = `
-    <div class="todo-main">
-        <!-- todo 输入框 -->
-        <div class="todo-form">
-            <input id='id-input-todo' type="text">
-            <button id='id-button-add' type="button">Add</button>
-            <input id='id-input-todoID' type='text'>
-            <button id='id-button-query' type='button'>Query</button>
-        </div>
-        <!-- todo 列表 -->
-        <div id="id-div-container">
-            <!-- <div class='todo-cell'>
-                <button class='todo-done'>完成</button>
-                <button class='todo-delete'>删除</button>
-                <span contenteditable='true'>上课</span>
-                <span>时间</span>
-                <span>ID</span>
-            </div> -->
-        </div>
-    </div>
-    `
-    $('body').append(todoContainer)
-}
-// todo-cell的模板
-var templateTodo = function(todo) {
-    /*   todo =
-    "created_time": 1476083080,
-    "id": 75,
-    "task": "to have dinner"
-    }*/
-    var t = `
-        <div class='todo-cell'>
-            <button class='todo-done'>完成</button>
-            <button class='todo-delete'>删除</button>
-            <button class='todo-edit'>编辑</button>
-            <span class='todo-label' contenteditable='false'>${todo.task}</span>
-            <span>${todo.created_time}</span>
-            <span>${todo.id}</span>
-        </div>
-    `
-    return t
-}
-// 插入一个todo-cell
-var insertTodo = function(todo) {
-    log('insertTodo 开始！')
-    var t = templateTodo(todo)
-    log('要添加的代码：', t)
-    // 添加到 container 中
-    $('#id-div-container').append(t)
-    log('添加结束')
-}
+// // 插入TODOlist区域
+// var todoList = function() {
+//     var todoContainer = `
+//     <div class="todo-main">
+//         <!-- todo 输入框 -->
+//         <div class="todo-form">
+//             <input id='id-input-todo' type="text">
+//             <button id='id-button-add' type="button">Add</button>
+//             <input id='id-input-todoID' type='text'>
+//             <button id='id-button-query' type='button'>Query</button>
+//         </div>
+//         <!-- todo 列表 -->
+//         <div id="id-div-container">
+//             <!-- <div class='todo-cell'>
+//                 <button class='todo-done'>完成</button>
+//                 <button class='todo-delete'>删除</button>
+//                 <span contenteditable='true'>上课</span>
+//                 <span>时间</span>
+//                 <span>ID</span>
+//             </div> -->
+//         </div>
+//     </div>
+//     `
+//     $('body').append(todoContainer)
+// }
+// // todo-cell的模板
+// var templateTodo = function(todo) {
+//     /*   todo =
+//     "created_time": 1476083080,
+//     "id": 75,
+//     "task": "to have dinner"
+//     }*/
+//     var t = `
+//         <div class='todo-cell'>
+//             <button class='todo-done'>完成</button>
+//             <button class='todo-delete'>删除</button>
+//             <button class='todo-edit'>编辑</button>
+//             <span class='todo-label' contenteditable='false'>${todo.task}</span>
+//             <span>${todo.id}</span>
+//         </div>
+//     `
+//     return t
+// }
+// // 插入一个todo-cell
+// var insertTodo = function(todo) {
+//     log('insertTodo 开始！')
+//     var t = templateTodo(todo)
+//     log('要添加的代码：', t)
+//     // 添加到 container 中
+//     $('#id-div-container').append(t)
+//     log('添加结束')
+// }
 
 
 // 使用阿贾克斯获取所有TODO task
@@ -72,18 +71,18 @@ var ajaxGetAll = function(){
     })
 }
 
-// 使用阿贾克斯post一个request
-var ajaxPostReq = function(){
+// 使用阿贾克斯post一个request, 添加todo
+var ajaxAddTodo = function(todo){
     var request = {
                 url: '/todo/add',
                 type: 'post',
                 contentType: 'application/json',
-                data: JSON.stringify({task: 'study', id: 1, created_time: 1234568}),
+                data: JSON.stringify(todo),
                 success: function(r) {
                     console.log('ok',arguments)
                     console.log('r--->', r)
                     var todo = JSON.parse(r)
-                    insertTodo(todo)
+                    // insertTodo(todo)
                 },
                 error: function() {
                     console.log('err',arguments)
@@ -92,6 +91,45 @@ var ajaxPostReq = function(){
     $.ajax(request)
 }
 
+// delete todo
+var ajaxDeleteTodo = function(todoId){
+    var request = {
+                url: '/todo/delete',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify({id: todoId}),
+                success: function(r) {
+                    console.log('ok',arguments)
+                    console.log('r--->', r)
+                    var todo = JSON.parse(r)
+                    // insertTodo(todo)
+                },
+                error: function() {
+                    console.log('err',arguments)
+                }
+            }
+    $.ajax(request)
+}
+
+// update todo
+var ajaxUpdateTodo = function(todo){
+    var request = {
+                url: '/todo/update',
+                type: 'post',
+                contentType: 'application/json',
+                data: JSON.stringify(todo),
+                success: function(r) {
+                    console.log('ok',arguments)
+                    console.log('r--->', r)
+                    var todo = JSON.parse(r)
+                    // insertTodo(todo)
+                },
+                error: function() {
+                    console.log('err',arguments)
+                }
+            }
+    $.ajax(request)
+}
 
 // 给 query button 绑定添加 todo 事件
 var bindEventQuery = function() {
@@ -106,7 +144,6 @@ var bindEventQuery = function() {
             log('查询全部')
             ajaxGetAll()
         }
-
         //else {
         //     log('todoID', todoID)
         //     todoGetTask(todoID)
@@ -121,5 +158,4 @@ var bindEventQuery = function() {
 var __main = function(){
     todoList()
     bindEventQuery()
-    ajaxPostReq()
 }
